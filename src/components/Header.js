184e94f8770fffeaa3e5c8a39a60e05843ccdef7
IngridGdesigns <garciai@protonmail.com>
2023-11-06 12:41:04 -0800

    added component folder and Header file

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div>
+            <header className="App-header">
+                <h1>my travel journal</h1>
+            </header>
+        </div>
+    )
+}
