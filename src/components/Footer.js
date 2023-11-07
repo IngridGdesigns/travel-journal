4bf127cb9560a4959cb8cd0610deec5e9624ac62
IngridGdesigns <garciai@protonmail.com>
2023-11-07 13:54:16 -0800

    added favicon and text to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Footer() {
     return (
         <div className='journal-footer'>
-            <caption> 2023</caption>
+            <caption>Ingrid G. 2023 |</caption>
+            <caption className='footer-caption'> Favicon by: </caption>
+            <a className='footer-caption' href="https://icons8.com/icon/xAjo5vjfr4Ov/coco-tree">Coco Tree</a> icon by <a href="https://icons8.com">Icons8</a>
 
         </div>
     )
