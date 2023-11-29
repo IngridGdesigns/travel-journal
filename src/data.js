16ab622c727f2bcbffc71692da89c7b51d0ed1c3
IngridGdesigns <garciai@protonmail.com>
2023-11-29 05:02:09 -0800

    changed dates

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const data = [
     {
         id: 1,
-        date: "May 2018",
+        date: "May 2022",
         title: "Panajachel, Lake Atitlán",
         description: "The busiest and most built-up lakeside town, Panajachel ('Pana') is the gateway to Lago de Atitlán for most travelers.",
         coverImg: "atitlan.jpeg",
@@ -10,7 +10,7 @@ export const data = [
     },
     {
         id: 2,
-        date: "December 2019",
+        date: "December 2021",
         title: "Secret Beach",
         description: "Experience the beautiful landscape and meet new friends all while enjoying pristine warm waves of the Atlantic.",
         coverImg: "belize.jpeg",
@@ -19,7 +19,7 @@ export const data = [
     },
        {
         id: 3,
-        date: "July 2022",
+        date: "July 2019",
         title: "León",
         description: "León long has been noted as a liberal political and intellectual centre of Nicaragua. There is no better place to brush up on your Spanish, take a salsa class or even just visit the local market.",
         coverImg: "nicaragua.jpeg",
